601082a9344e6a43ab4bf1c6899be85bcd6c1fcc
steuer.konstantin <steuer.konstantin@swm.de>
2014-11-10 10:40:38 +0100

    CorsFilter verbessert

--- backend/server/src/main/java/org/mofd/jsevaluation/backend/server/filter/CorsFilter.java
+++ backend/server/src/main/java/org/mofd/jsevaluation/backend/server/filter/CorsFilter.java
@@ -11,47 +11,80 @@ import java.io.IOException;
 import java.net.URL;
 
 /**
- * TODO: Document me
+ * CorsFilter.
  *
  * @author konstantinsteuer
  * @since 09.11.14
  */
 @Component("corsFilter")
 public class CorsFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
 
-    }
+	private static final int HTTPS_DEFAULT_PORT = 443;
+	private static final int HTTP_DEFAULT_PORT = 80;
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        String corsHeader = createOriginUrl(request);
-        if (corsHeader != null) {
-            response.addHeader("Access-Control-Allow-Origin", corsHeader);
-        }
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
+	static final String ORIGIN_HEADER = "Origin";
+	static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+	static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+	static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+	static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+	static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
 
-    private String createOriginUrl(HttpServletRequest request) {
-        String origin = request.getHeader("Origin");
-        if (StringUtils.isEmpty(origin)) {
-            return null;
-        } else {
-            try {
-                URL url = new URL(origin);
-                return url.getProtocol() + "://" + url.getHost() + ":" + url.getPort();
-            } catch (Exception e) {
-                return request.getRequestURL().toString();
-            }
-        }
-    }
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
 
-    @Override
-    public void destroy() {
+	}
 
-    }
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		String originUrl = getOriginUrl(request);
+		if (originUrl != null) {
+			response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, originUrl);
+			String methods = request.getHeader(ACCESS_CONTROL_REQUEST_METHOD);
+			if (!StringUtils.isEmpty(methods)) {
+				response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, methods);
+			}
+			String headers = request.getHeader(ACCESS_CONTROL_REQUEST_HEADERS);
+			if (!StringUtils.isEmpty(headers)) {
+				response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, headers);
+			}
+		}
+
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
+
+	private String getOriginUrl(HttpServletRequest request) {
+		String origin = request.getHeader(ORIGIN_HEADER);
+		if (StringUtils.isEmpty(origin)) {
+			return null;
+		} else {
+			try {
+				URL originUrl = new URL(origin);
+				StringBuilder url = new StringBuilder();
+				String scheme = originUrl.getProtocol();
+				int port = originUrl.getPort();
+
+				url.append(scheme);
+				url.append("://");
+				url.append(originUrl.getHost());
+				if (port > 0 && ((scheme.equalsIgnoreCase("http") && port != HTTP_DEFAULT_PORT) || (scheme
+					.equalsIgnoreCase("https") && port != HTTPS_DEFAULT_PORT))) {
+					url.append(":");
+					url.append(port);
+				}
+
+				return url.toString();
+			} catch (Exception e) {
+				return origin;
+			}
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
 
 
 }
