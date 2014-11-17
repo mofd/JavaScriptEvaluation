477f3219e94ef5fb64e01377b663b1e586b7ea09
steuer.konstantin <steuer.konstantin@swm.de>
2014-11-17 15:43:21 +0100

    client * default-validierung * Styling

--- client/platform/scripts/register/register.js
+++ client/platform/scripts/register/register.js
@@ -2,20 +2,25 @@
 ///<reference path="../configuration/configuration.ts"/>
 "use strict";
 platform.controller("RegisterCtrl", function ($scope, $http, $location, configurationService) {
-    $scope.doRegister = function () {
-        var register = JSON.stringify({
-            name: $scope.name,
-            vorname: $scope.vorname,
-            mail: $scope.mail,
-            benutzername: $scope.benutzername,
-            passwort: $scope.passwort
-        });
-        $http.post(configurationService.getCurrentConfiguration().serverUrl + "register/", register).success(function (data, status, headers, config) {
-            $location.url("/login");
-        }).error(function (data, status, headers, config) {
-            console.log("Registrierung ist fehlgeschlagen: " + data);
-            alert('Registrierung ist fehlgeschlagen');
-        });
+    $scope.doRegister = function (valide) {
+        if (valide) {
+            var register = JSON.stringify({
+                name: $scope.name,
+                vorname: $scope.vorname,
+                mail: $scope.mail,
+                benutzername: $scope.benutzername,
+                passwort: $scope.passwort
+            });
+            $http.post(configurationService.getCurrentConfiguration().serverUrl + "register/", register).success(function (data, status, headers, config) {
+                $location.url("/login");
+            }).error(function (data, status, headers, config) {
+                console.log("Registrierung ist fehlgeschlagen: " + data);
+                alert('Registrierung ist fehlgeschlagen');
+            });
+        }
+        else {
+            $scope.submitted = true;
+        }
     };
 });
 //# sourceMappingURL=register.js.map
--- client/platform/scripts/register/register.ts
+++ client/platform/scripts/register/register.ts
@@ -10,26 +10,31 @@ module register {
         mail:string;
         benutzername:string;
         passwort:string;
+        submitted:boolean;
 
-        doRegister():void;
+        doRegister(valide:boolean):void;
     }
 }
 
 platform.controller("RegisterCtrl", function ($scope:register.IRegisterScope, $http:ng.IHttpService, $location,
                                               configurationService:configuration.IConfigurationService) {
 
-    $scope.doRegister = function () {
-        var register = JSON.stringify({
-            name: $scope.name, vorname: $scope.vorname, mail: $scope.mail,
-            benutzername: $scope.benutzername, passwort: $scope.passwort
-        });
-        $http.post(configurationService.getCurrentConfiguration().serverUrl + "register/", register)
-            .success(function (data:number, status, headers, config) {
-                $location.url("/login");
-            })
-            .error(function (data, status, headers, config) {
-                console.log("Registrierung ist fehlgeschlagen: " + data)
-                alert('Registrierung ist fehlgeschlagen');
-            })
+    $scope.doRegister = function (valide:boolean) {
+        if(valide) {
+            var register = JSON.stringify({
+                name: $scope.name, vorname: $scope.vorname, mail: $scope.mail,
+                benutzername: $scope.benutzername, passwort: $scope.passwort
+            });
+            $http.post(configurationService.getCurrentConfiguration().serverUrl + "register/", register)
+                .success(function (data:number, status, headers, config) {
+                    $location.url("/login");
+                })
+                .error(function (data, status, headers, config) {
+                    console.log("Registrierung ist fehlgeschlagen: " + data)
+                    alert('Registrierung ist fehlgeschlagen');
+                })
+        } else {
+            $scope.submitted = true;
+        }
     };
 });
