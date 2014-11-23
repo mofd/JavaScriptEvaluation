913113cf18c5114c125020ef92d26a347b89f051
mofd <technology@email.de>
2014-11-23 13:54:00 +0100

    client * erste schritte zum amd

---
+++ client/app/scripts/main.ts
@@ -0,0 +1,46 @@
+///<reference path='../../typed/angularjs/angular.d.ts' />
+///<reference path='../../typed/requirejs/require.d.ts' />
+
+require.config({
+    baseUrl: '',
+    paths: {
+        'jquery': '../lib/jquery/dist/jquery.min',
+        'angular': '../lib/angular/angular.min',
+        'angular-route': '../lib/angular-route/angular-route.min',
+        'angular-resource': '../lib/angular-resource/angular-resource.min',
+
+        'platform': '../platform/scripts/platform',
+        'platform-dispatcher': '../platform/scripts/dispatcher/dispatcher',
+        'platform-configuration': '../platform/scripts/configuration/configuration',
+        'platform-navigation': '../platform/scripts/navigation/navigation',
+        'platform-login': '../platform/scripts/login/login',
+        'platform-register': '../platform/scripts/register/register',
+    },
+
+    shim: {
+        'angular': {
+            exports: 'angular',
+            deps: ['jquery']
+        },
+        'angular-route': ['angular'],
+        'angular-resource': ['angular'],
+
+        'platform': { exports: 'platform' },
+        'platform-dispatcher': { exports: 'platform-dispatcher', dep: ['platform'] },
+        'platform-configuration': { exports: 'platform-configuration', dep: ['platform'] },
+        'platform-navigation': { exports: 'platform-navigation', dep: ['platform'] },
+
+        'platform-login': { exports: 'platform-login', dep: ['platform'] },
+        'platform-register': { exports: 'platform-register', dep: ['platform'] },
+    }
+});
+
+// startup the application
+require(['angular', 'platform', 'platform-dispatcher', 'platform-navigation', 'platform-configuration'],
+
+    function (angular:ng.IAngularStatic, document) {
+
+        // bootstrap the document, since we are loading asynchronously
+        angular.bootstrap(document, ['app']);
+    }
+);
